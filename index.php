8474c3f500a0f4f246fad2de0d0de80826f42f42
JurrijnP <slubadog@gmail.com>
2018-04-11 15:39:07 +0200

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>EchoAPI</title>
-        <script>
+        <script type="text/javascript">
             document.getElementById("Hi").innerHTML = "Hello";
         </script>
     </head>
